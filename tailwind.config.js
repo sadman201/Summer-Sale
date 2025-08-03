33421368a3dbee55f93721fc5b55b185f61e2585
sadman201 <saidulsadman100@gmail.com>
2025-08-03 14:17:25 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1DA1F2",
+        secondary: "#14171A",
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
