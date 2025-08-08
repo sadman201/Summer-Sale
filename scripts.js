0ff072157df29b8c3069e5742ff3bb81cd30c87f
sadman201 <saidulsadman100@gmail.com>
2025-08-08 19:31:47 +0600

    updated code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,24 @@
 const nameList = document.getElementById("nameList");
-const Total = document.getElementById("Total");
-const Discount = document.getElementById("Discount");
+const total = document.getElementById("total");
+const discountTotal = document.getElementById("discountTotal");
+const purchaseBtn = document.getElementById("purchaseBtn");
+const applyBtn = document.getElementById("applyBtn");
+const inputCoupon = document.getElementById("inputCoupon");
+const discountContainer = document.getElementById("discount");
+ applyBtn.disabled = true;
+purchaseBtn.disabled = true;
+
 let totalPrice = 0;
+if  (totalPrice <= 0){
+
+  applyBtn.classList.add("bg-gray-400");
+
+}
+ if (totalPrice <= 0){
+   purchaseBtn.classList.add("bg-gray-400");
+  }
+  
+
 const handleClick = (data) => {
  const name = data.childNodes[1].childNodes[5].innerText;
   const priceAndTk = data.childNodes[1].childNodes[7].innerText;
@@ -10,11 +27,52 @@ const handleClick = (data) => {
   
 
   totalPrice = totalPrice + priceNumber;
-    Total.innerText = totalPrice;
-    DiscountTotal.innerText = totalPrice;
+    total.innerText = totalPrice;
+    discountTotal.innerText = totalPrice;
 
   const li = document.createElement("li");
   li.innerText = name;
   nameList.appendChild(li);
+  if ( totalPrice < 0) {
+    applyBtn.disabled = false;
+  }
+  if (totalPrice  > 0 ){
+    purchaseBtn.disabled = false;
+     purchaseBtn.classList.add("bg-pink-400");
+  }
+  if (totalPrice >= 200){
+    applyBtn.disabled = false;
+    applyBtn.classList.add("bg-pink-400")
+  }
 
 };
+
+
+applyBtn.addEventListener("click", () => { 
+  if (inputCoupon.value === ""){
+    alert("Input is required");
+    return;
+  }
+   
+  if (inputCoupon.value === "SELL200" ){
+
+    const discounRate = 0.2;
+   const discount = totalPrice * discounRate;
+  const totalPriceWidthDiscount = totalPrice - discount;
+  discountContainer.innerText = discount.toFixed(2);
+  discountTotal.innerText = totalPriceWidthDiscount;
+  }else {
+    alert("Coupon Not match");
+  }
+   
+})
+
+
+purchaseBtn.addEventListener("click",() => {
+  totalPrice = 0;
+ total.innerText = "00";
+  discountTotal.innerText = "00";
+  discountContainer.innerText = "00";
+  nameList.innerHTML = "";
+
+}) 
